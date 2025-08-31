d6f9e0bd6f31ae34898f49a001de65337f3522cd
LeenaPillai <leepilla@in.ibm.com>
2025-08-31 14:38:12 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,5 @@
+	
+myname = input( "What is your name: " )
+myage = int( input( "How old are you: " ) )
+year = str( ( 2025 - myage ) + 100 )
+print( myname + " will be 100 years old in the year " + year )
