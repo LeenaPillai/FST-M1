561ea9db46b0f3dbeab64c305eeb29fe80aecd58
LeenaPillai <leepilla@in.ibm.com>
2025-08-31 15:07:02 +0530

    Add files via upload

---
+++ Python/Activities/Activity8.py
@@ -0,0 +1,15 @@
+	
+# Given list of numbers
+numList = [30, 50, 20, 10, 40]
+print("Given list is ", numList)
+ 
+# Get first element in list
+firstElement = numList[0]
+# Get last element in list
+lastElement = numList[-1]
+ 
+# Check if first and last element are equal
+if (firstElement == lastElement):
+    print(True)
+else:
+    print(False)
